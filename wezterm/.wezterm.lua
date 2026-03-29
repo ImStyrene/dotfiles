231e540df83edb6322f0eaaf09b68f97f6323a5d
ImStyrene <imstyrene@protonmail.com>
2026-03-29 17:01:28 -0400

    Changed font and added semicolons

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,40 +1,37 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
+local wezterm = require("wezterm");
+local config = wezterm.config_builder();
 
 -- # STARTUP # --
 config.default_prog = {
-  "/run/current-system/sw/bin/zsh",
-  "-c",
-  "fastfetch; source ~/.zshrc; exec zsh",
-}
+  "/run/current-system/sw/bin/zsh";
+  "-c";
+  "fastfetch; source ~/.zshrc; exec zsh";
+};
 
 -- # FONT # --
-config.font = wezterm.font("HackNerdFont", {
-  weight = "Regular",
-  style = "Normal",
-})
-config.font_size = 12.0
+config.font = wezterm.font("CaskaydiaCoveNerdFont");
+config.font_size = 12.0;
+config.line_height = 1.05;
 
 -- # STYLE # --
-config.window_background_opacity = 0.9
-config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.9;
+config.hide_tab_bar_if_only_one_tab = true;
 config.window_padding = {
-  left   = 0,
-  right  = 0,
-  top    = 0,
-  bottom = 0,
-}
+  left   = 0;
+  right  = 0;
+  top    = 0;
+  bottom = 0;
+};
 
 -- # BLUR # --
 -- KDE Plasma
-config.kde_window_background_blur = true
+config.kde_window_background_blur = true;
 
 -- # COLOR SCHEME # --
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "Tokyo Night Moon";
 
 -- # HOTKEYS # --
 config.keys = {
-}
+};
 
 return config
-
